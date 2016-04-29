43fcffbc1328a96a995b9032fd0c8ca66dfac6cf
Daniel Lemire <lemire@gmail.com>
2016-04-29 17:07:14 -0400

    Documenting change.

--- scripts/all.sh
+++ scripts/all.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH=$DIR/../roaringlib/:$LD_LIBRARY_PATH
 declare -a commands=('bitmagic_benchmarks' 'roaring_benchmarks -r' 'roaring_benchmarks' 'ewah32_benchmarks'  'ewah64_benchmarks' );
-echo "# For data set, we print file size (in bytes), successive intersections, successive unions and total unions (in cycles)"
+echo "# For data set, we print file size (in bytes), successive intersections, successive unions and total unions (in cycles) [for roaring we also report the heap-based total union time in cycles]"
 for f in census1881_srt  census-income_srt census1881      census-income weather_sept_85_srt  wikileaks-noquotes_srt  wikileaks-noquotes; do
   echo "# processing file " $f
   for t in "${commands[@]}"; do
