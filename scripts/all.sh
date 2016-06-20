6011abaeb60fe451a5a697758422cafaeaa981f1
Owen Kaser <o.kaser@computer.org>
2016-06-20 14:59:43 -0300

    tweaked the column description comment in all.sh

--- scripts/all.sh
+++ scripts/all.sh
@@ -5,7 +5,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH=$DIR/../roaringlib/:$LD_LIBRARY_PATH
 declare -a commands=('bitset_benchmarks' 'stl_vector_benchmarks' 'stl_hashset_benchmarks' 'bitmagic_benchmarks'  'bitmagic_benchmarks -r' 'roaring_benchmarks -r' 'roaring_benchmarks -c -r' 'roaring_benchmarks' 'roaring_benchmarks -c'  'ewah32_benchmarks'  'ewah64_benchmarks' 'wah32_benchmarks' 'concise_benchmarks' );
-echo "# For each data set, we print data size (in bits per value), successive intersections, successive unions and total unions, followed by quartile point queries (in cycles per input value) [we compute the total  union either naively or using a heap-based approach]"
+echo "# For each data set, we print data size (in bits per value), successive intersections, successive unions and total unions [we compute the total  union first naively and then (if supported) using a heap-based approach], followed by quartile point queries (in cycles per input value) "
 for f in census1881_srt  census-income_srt census1881      census-income weather_sept_85_srt  wikileaks-noquotes_srt  wikileaks-noquotes; do
   echo "# processing file " $f
   for t in "${commands[@]}"; do
