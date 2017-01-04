23b221ada002a8790e3b70da434d66250ec21779
Daniel Lemire <lemire@gmail.com>
2017-01-04 14:57:32 -0500

    Adding ANDNOT and XOR to bitmagic benchmark

--- src/bitmagic_benchmarks.cpp
+++ src/bitmagic_benchmarks.cpp
@@ -120,7 +120,7 @@ int main(int argc, char **argv) {
     const char *extension = ".txt";
     bool verbose = false;
     bool memorysavingmode = false;
-    uint64_t data[6];
+    uint64_t data[8];
     while ((c = getopt(argc, argv, "rve:h")) != -1) switch (c) {
         case 'e':
             extension = optarg;
@@ -252,16 +252,50 @@ int main(int argc, char **argv) {
     if(verbose) printf("Quartile queries on %zu bitmaps took %" PRIu64 " cycles\n", count,
            data[5]);
 
+    /**
+    * andnot and xor
+    */
+
+    uint64_t successive_andnot = 0;
+    uint64_t successive_xor = 0;
+
+    RDTSC_START(cycles_start);
+    for (int i = 0; i < (int)count - 1; ++i) {
+        bvect tempandnot = bitmaps[i] - bitmaps[i + 1];
+        successive_andnot += tempandnot.count();
+    }
+    RDTSC_FINAL(cycles_final);
+    data[6] = cycles_final - cycles_start;
+    if(verbose) printf("Successive differences on %zu bitmaps took %" PRIu64 " cycles\n", count,
+                           cycles_final - cycles_start);
+
+    RDTSC_START(cycles_start);
+    for (int i = 0; i < (int)count - 1; ++i) {
+        bvect tempxor = bitmaps[i] ^ bitmaps[i + 1];
+        successive_xor += tempxor.count();
+    }
+    RDTSC_FINAL(cycles_final);
+    data[7] = cycles_final - cycles_start;
+    if(verbose) printf("Successive symmetric differences on %zu bitmaps took %" PRIu64 " cycles\n", count,
+                           cycles_final - cycles_start);
+
+
+    /**
+    * end of andnot and xor
+    */
+
     if(verbose) printf("Collected stats  %" PRIu64 "  %" PRIu64 "  %" PRIu64 " %" PRIu64 "\n",successive_and,successive_or,total_or,quartcount);
 
-    printf(" %20.2f %20.2f %20.2f %20.2f %20.2f %20.2f \n",
+    printf(" %20.2f %20.2f %20.2f %20.2f %20.2f %20.2f %20.2f %20.2f \n",
       data[0]*8.0/totalcard,
       data[1]*1.0/successivecard,
       data[2]*1.0/successivecard,
       data[3]*1.0/totalcard,
       data[4]*1.0/totalcard,
-      data[5]*1.0/(3*count)
-    );
+      data[5]*1.0/(3*count),
+      data[6]*1.0/successivecard,
+      data[7]*1.0/successivecard
+     );
 
 
     for (int i = 0; i < (int)count; ++i) {
